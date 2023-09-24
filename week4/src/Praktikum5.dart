be39cb3cc874eb6199eb75e40c8be412449df319
Febrian Dani <87335182+daniertg@users.noreply.github.com>
2023-09-25 05:37:45 +0700

    Update Praktikum5.dart

--- week4/src/Praktikum5.dart
+++ week4/src/Praktikum5.dart
@@ -1,12 +1,19 @@
 void main(){
-  var record = ('first', a: 2, b: true, 'last');
-  print(record);
+  // var record = ('first', a: 2, b: true, 'last');
+  // print(record);
   
   (int, int) tukar((int, int) record) {
   var (a, b) = record;
   return (b, a);
 }
 // Record type annotation in a variable declaration:
-(String, int) mahasiswa;
+(String, int) mahasiswa= ('Febrian dani', 214172007);
 print(mahasiswa);
+
+var mahasiswa2 = ('first', a: 2, b: true, 'last');
+
+print(mahasiswa2.$1); // Prints 'first'
+print(mahasiswa); // Prints 2
+print(mahasiswa2.b); // Prints true
+print(mahasiswa2.$2); // Prints 'last'
 }
